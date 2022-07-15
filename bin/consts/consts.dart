e4043ef34bd3baf3bb396ba018d14a4ff2a2ca61
Axmadjon Isaqov <67532499+axmadjon2000@users.noreply.github.com>
2022-07-15 15:32:57 +0500

    comitted

--- bin/consts/consts.dart
+++ bin/consts/consts.dart
@@ -13,25 +13,5 @@ class MyConsts {
         author: 'Alberto miola',
         id: '01',
         link: 'https://t.me/flutter_G_Two/50'),
-    Book(
-        name: "Flutter Complate",
-        author: 'Alberto miola',
-        id: '02',
-        link: 'https://t.me/flutter_G_Two/50'),
-    Book(
-        name: "Flutter Complate",
-        author: 'Alberto miola',
-        id: '03',
-        link: 'https://t.me/flutter_G_Two/50'),
-    Book(
-        name: "Flutter Complate",
-        author: 'Alberto miola',
-        id: '04',
-        link: 'https://t.me/flutter_G_Two/50'),
-    Book(
-        name: "Flutter Complate",
-        author: 'Alberto miola',
-        id: '05',
-        link: 'https://t.me/flutter_G_Two/50'),
   ];
 }
